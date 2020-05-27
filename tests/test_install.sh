e3c07c54b9e55bf147343a704a5b9d604004a29f
Egor Dranischnikow <egor.dranischnikow@googlemail.com>
2020-05-27 22:41:40 +0200

    adding possibility to install from pypi for testing

--- tests/test_install.sh
+++ tests/test_install.sh
@@ -10,10 +10,15 @@ echo "Testing python3"
 
 #prepare:
 pip install cython
+pip install numpy
 pip install https://github.com/realead/uttemplate/zipball/master
 
 if [ "$1" = "from-github" ]; then
     pip install https://github.com/realead/cykhash/zipball/master
+elif [ "$1" = "from-test-pypi" ]; then
+    pip install -i https://test.pypi.org/simple/ cykhash
+elif [ "$1" = "from-pypi" ]; then
+    pip install cykhash
 else
     (cd .. && python setup.py install)
 fi;

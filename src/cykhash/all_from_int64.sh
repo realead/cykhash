a5ca5338de322762737c94f5c6d7e41a1faeb87c
Egor Dranischnikow <egor.dranischnikow@googlemail.com>
2020-12-14 22:57:31 +0100

    removing maps from all_from_int64.sh

--- src/cykhash/all_from_int64.sh
+++ src/cykhash/all_from_int64.sh
@@ -78,51 +78,5 @@ sed -i -- 's/Int64/PyObject/g'   $TARGET_DIR/pyobjectset_impl.pxi
 sed -i -- 's/int64_t/object/g'   $TARGET_DIR/pyobjectset_impl.pxi
 
 
-###  MAPS
-
-SOURCE_DIR="maps/int64/"
-MAP_DIR="maps/"
-
-
-### int32map
-echo "Creating int32map from int64map..."
-TARGET_DIR=$MAP_DIR/int32/
-# mkdir -p $TARGET_DIR
-cp $SOURCE_DIR/int64to64map_header.pxi  $TARGET_DIR/int32to32map_header.pxi
-sed -i -- 's/64/32/g'   $TARGET_DIR/int32to32map_header.pxi
-cp $SOURCE_DIR/int64to64map_impl.pxi    $TARGET_DIR/int32to32map_impl.pxi
-sed -i -- 's/64/32/g'   $TARGET_DIR/int32to32map_impl.pxi
-
-
-### float64map
-echo "Creating float64map from int64map..."
-TARGET_DIR=$MAP_DIR/float64/
-# mkdir -p $TARGET_DIR
-cp $SOURCE_DIR/int64to64map_header.pxi  $TARGET_DIR/float64to64map_header.pxi
-sed -i -- 's/ctypedef int64_t key_int64_t/ctypedef float64_t key_float64_t/g'   $TARGET_DIR/float64to64map_header.pxi
-sed -i -- 's/key_int64_t/key_float64_t/g'   $TARGET_DIR/float64to64map_header.pxi
-sed -i -- 's/int64to64/float64to64/g'   $TARGET_DIR/float64to64map_header.pxi
-sed -i -- 's/Int64to64Map/Float64to64Map/g'   $TARGET_DIR/float64to64map_header.pxi
-sed -i -- 's/_int64map/_float64map/g'   $TARGET_DIR/float64to64map_header.pxi
-cp $SOURCE_DIR/int64to64map_impl.pxi    $TARGET_DIR/float64to64map_impl.pxi
-sed -i -- 's/ctypedef int64_t key_int64_t/ctypedef float64_t key_float64_t/g'   $TARGET_DIR/float64to64map_impl.pxi
-sed -i -- 's/key_int64_t/key_float64_t/g'   $TARGET_DIR/float64to64map_impl.pxi
-sed -i -- 's/int64to64/float64to64/g'   $TARGET_DIR/float64to64map_impl.pxi
-sed -i -- 's/Int64to64Map/Float64to64Map/g'   $TARGET_DIR/float64to64map_impl.pxi
-sed -i -- 's/_int64map/_float64map/g'   $TARGET_DIR/float64to64map_impl.pxi
-
-
-
-SOURCE_DIR="maps/float64/"
-
-### float32map
-echo "Creating float32map from float64map..."
-TARGET_DIR=$MAP_DIR/float32/
-# mkdir -p $TARGET_DIR
-cp $SOURCE_DIR/float64to64map_header.pxi  $TARGET_DIR/float32to32map_header.pxi
-sed -i -- 's/64/32/g'   $TARGET_DIR/float32to32map_header.pxi
-cp $SOURCE_DIR/float64to64map_impl.pxi    $TARGET_DIR/float32to32map_impl.pxi
-sed -i -- 's/64/32/g'   $TARGET_DIR/float32to32map_impl.pxi
-
 
 

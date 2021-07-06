86f8287c4c9f23c980db4088369e5f0166e3f0b6
Egor Dranischnikow <egor.dranischnikow@googlemail.com>
2021-07-06 20:32:09 +0200

    different results  on 32 and 64bit

--- src/cykhash/compat.py
+++ src/cykhash/compat.py
@@ -1,6 +1,19 @@
+import sys
 import platform
+
+
 
 PYPY = platform.python_implementation() == "PyPy"
 
 def assert_if_not_on_PYPY(statement, reason):
     assert PYPY or statement
+
+
+
+IS64BIT = sys.maxsize > 2 ** 32
+
+def assert_equal_32_or_64(val, expected32, expected64, reason):
+    if IS64BIT:
+        assert val == expected64
+    else:
+        assert val == expected32
--- tests/unit_tests/test_utils.py
+++ tests/unit_tests/test_utils.py
@@ -2,7 +2,7 @@ from unittestmock import UnitTestMock
 
 from cykhash.utils import float64_hash, float32_hash, int64_hash, int32_hash, object_hash, objects_are_equal
 
-from cykhash.compat import assert_if_not_on_PYPY
+from cykhash.compat import assert_if_not_on_PYPY, assert_equal_32_or_64
 
 class TestUtils(UnitTestMock): 
     def test_hash_float64_neg_zero(self):
@@ -14,7 +14,12 @@ class TestUtils(UnitTestMock):
 
 
     def test_hash_float64_one(self):
-        self.assertEqual(float64_hash(1.0), 1954243739)
+        assert_equal_32_or_64(
+                     val=float64_hash(1.0),
+                     expected32=741795711,
+                     expected64=1954243739,
+                     reason="different murmur implementations"
+        )
 
 
     def test_hash_float32_one(self):
@@ -22,7 +27,12 @@ class TestUtils(UnitTestMock):
 
 
     def test_hash_int64_zero(self):
-        self.assertEqual(int64_hash(0), 4178429809)
+        assert_equal_32_or_64(
+                     val=int64_hash(0),
+                     expected32=224447722,
+                     expected64=4178429809,
+                     reason="different murmur implementations"
+        )
 
 
     def test_hash_int32_zero(self):
@@ -30,7 +40,12 @@ class TestUtils(UnitTestMock):
 
 
     def test_hash_int64_one(self):
-        self.assertEqual(int64_hash(1), 1574219535)
+        assert_equal_32_or_64(
+                     val=int64_hash(1),
+                     expected32=190876766,
+                     expected64=1574219535,
+                     reason="different murmur implementations"
+        )
 
 
     def test_hash_int32_one(self):
@@ -43,6 +58,12 @@ class TestUtils(UnitTestMock):
 
     def test_hash_object_one(self):
         self.assertEqual(object_hash(1), 2049)
+        assert_equal_32_or_64(
+                     val=object_hash(1),
+                     expected32=1,
+                     expected64=2049,
+                     reason="no additional hashing for 32bit and pyobjects"
+        )
 
 
     def test_equal_tupples_with_nan(self):
